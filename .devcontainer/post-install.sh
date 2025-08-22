308349454649991753b583fd75880738a01178fa
Elias Paknejad <627105+elpadev@users.noreply.github.com>
2025-08-22 22:21:32 +0200

    Add `CronJob` Kind (#2)

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -2,3 +2,14 @@
 set -x
 
 echo "Running the post-install.sh script"
+
+# Install latest version of kubebuilder
+echo "Installing kubebuilder..."
+# download kubebuilder and install locally.
+curl -L -o kubebuilder "https://go.kubebuilder.io/dl/latest/$(go env GOOS)/$(go env GOARCH)"
+chmod +x kubebuilder && sudo mv kubebuilder /usr/local/bin/
+kubebuilder version
+
+go mod tidy # Clean up go.mod and go.sum files
+
+exit 0
